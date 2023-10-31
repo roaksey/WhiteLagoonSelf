51f1a25cfc9157568204e2de081476adb6a088c1
RajeevKumarMahato <rajeev@teksewa.com>
2023-10-31 20:52:34 +0545

    VIlla Number update and delete with view and action

--- WhiteLagoon.Web/Views/Home/VillaNumberController.cs
+++ WhiteLagoon.Web/Views/Home/VillaNumberController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
+using System.Security.Principal;
 using WhiteLagoon.Domain.Entities;
 using WhiteLagoon.Infrastructure.Data;
 using WhiteLagoon.Web.ViewModels;
@@ -39,17 +40,88 @@ namespace WhiteLagoon.Web.Views.Home
             return View(vm);
         }
         [HttpPost]
-        public IActionResult Create(VillaNumber obj)
+        public IActionResult Create(VillaNumberVM obj)
         {
+            bool roomNumExist = _db.VillaNumbers.Any(x => x.Villa_Number == obj.VillaNumber.Villa_Number);
             //ModelState.Remove("Villa");
-            if (ModelState.IsValid)
+            if (ModelState.IsValid && !roomNumExist)
             {
-                _db.VillaNumbers.Add(obj);
+                _db.VillaNumbers.Add(obj.VillaNumber);
                 _db.SaveChanges();
                 TempData["success"] = "Villa Number has been created successfully.";
                 return RedirectToAction(nameof(Index));
             }
+            if (roomNumExist)
+            {
+                TempData["error"] = "Villa Number " + obj.VillaNumber.Villa_Number + " already exists.";
+            }
+            obj.VillaList = _db.Villas.Select(x => new SelectListItem
+            {
+                Value = x.Id.ToString(),
+                Text = x.Name
+            });
             return View(obj);
+        }
+
+        public IActionResult Update(int villaNumId)
+        {
+            VillaNumberVM model = new() {
+                VillaNumber = _db.VillaNumbers.FirstOrDefault(x => x.Villa_Number == villaNumId),
+                VillaList = _db.Villas.Select(x=> new SelectListItem { Value = x.Id.ToString(), Text = x.Name })   
+            };
+            if (model.VillaNumber is null)
+            {
+                TempData["error"] = "Could not find villa Number";
+                return RedirectToAction("Error", "Home");
+            }
+            return View(model);
+        }
+        [HttpPost]
+        public IActionResult Update(VillaNumberVM model)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.VillaNumbers.Update(model.VillaNumber);
+                _db.SaveChanges();
+                TempData["success"] = "Villa Number updated successfully";
+                return RedirectToAction(nameof(Index));
+            }
+            model.VillaList = _db.Villas.Select(x => new SelectListItem
+            {
+                Value = x.Id.ToString(),
+                Text = x.Name
+            });
+            return View(model);
+        }
+        public IActionResult Delete(int villaNumId)
+        {
+            VillaNumberVM model = new()
+            {
+                VillaNumber = _db.VillaNumbers.FirstOrDefault(x => x.Villa_Number == villaNumId),
+                VillaList = _db.Villas.Select(x => new SelectListItem { Value = x.Id.ToString(), Text = x.Name })
+            };
+            if (model.VillaNumber is null)
+            {
+                TempData["error"] = "Could not find villa Number.";
+                return RedirectToAction("Error", "Home");
+            }
+            return View(model);
+        }
+
+        [HttpPost]
+        public IActionResult Delete(VillaNumberVM model)
+        {
+            VillaNumber? objFromDb = _db.VillaNumbers
+                 .FirstOrDefault(u => u.Villa_Number == model.VillaNumber.Villa_Number);
+            if (objFromDb is not null)
+            {
+                _db.VillaNumbers.Remove(objFromDb);
+                _db.SaveChanges();
+                TempData["success"] = "The villa number has been deleted successfully.";
+                return RedirectToAction("Index");
+            }
+            TempData["error"] = "The villa number could not be deleted.";
+            return View();
         }
     }
 }
