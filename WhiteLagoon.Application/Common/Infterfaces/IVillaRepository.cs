ad712d74b3cfa16617f76887edc064d024abf3bb
RajeevKumarMahato <rajeev@teksewa.com>
2023-11-24 12:09:27 +0545

    Generic Repository in Action

--- WhiteLagoon.Application/Common/Infterfaces/IVillaRepository.cs
+++ WhiteLagoon.Application/Common/Infterfaces/IVillaRepository.cs
@@ -8,13 +8,9 @@ using WhiteLagoon.Domain.Entities;
 
 namespace WhiteLagoon.Application.Common.Infterfaces
 {
-    public interface IVillaRepository
-    {
-        IEnumerable<Villa> GetAll(Expression<Func<Villa,bool>>? filter = null,string? includeProperties = null);
-        Villa Get(Expression<Func<Villa, bool>> filter, string? includeProperties = null);
-        void Add(Villa entity); 
-        void Update(Villa entity);
-        void Remove(Villa entity);
+    public interface IVillaRepository:IRepository<Villa>
+    {   
+        void Update(Villa entity);       
         void Save();
     }
 }
