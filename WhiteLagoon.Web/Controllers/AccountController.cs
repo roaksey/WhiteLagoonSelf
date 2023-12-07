bf3bedfa3dc25ba9326bb027dc3bba493eed67b1
roaksey <roaksey@gmail.com>
2023-12-07 13:06:53 +0545

    Section 11 - Add Account Controller and Inject Helper Dependencies  master

---
+++ WhiteLagoon.Web/Controllers/AccountController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using WhiteLagoon.Application.Common.Infterfaces;
+using WhiteLagoon.Domain.Entities;
+
+namespace WhiteLagoon.Web.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly SignInManager<ApplicationUser> _signInManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+        public AccountController(IUnitOfWork unitOfWork,UserManager<ApplicationUser> userManager,SignInManager<ApplicationUser> signInManager
+            ,RoleManager<IdentityRole> roleManager)
+        {
+            _unitOfWork = unitOfWork;
+            _userManager = userManager;
+            _signInManager = signInManager;
+            _roleManager = roleManager;
+        }
+        public IActionResult Login()
+        {
+            return View();
+        }
+        public IActionResult Register()
+        {
+            return View();
+        }
+    }
+}
