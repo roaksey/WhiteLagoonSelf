05bc4be33a2a88b59247644b725175ef146e03e7
roaksey <roaksey@gmail.com>
2024-01-03 14:06:41 +0545

    Section 13 - Get Action Finalize Booking

--- WhiteLagoon.Domain/Entities/Booking.cs
+++ WhiteLagoon.Domain/Entities/Booking.cs
@@ -34,9 +34,9 @@ namespace WhiteLagoon.Domain.Entities
         [Required]
         public DateTime BookingDate { get; set; }
         [Required]
-        public DateTime CheckInDate { get; set; }
+        public DateOnly CheckInDate { get; set; }
         [Required]
-        public DateTime CheckOutDate { get; set;}
+        public  DateOnly CheckOutDate { get; set;}
 
         public bool IsPaymentSuccessful { get; set; } = false;
         public DateTime PaymentDate { get; set; }
