c2739d1057aaa06b031e752ee77a5405b86ec0d5
roaksey <roaksey@gmail.com>
2024-01-22 12:53:27 +0545

    Section 16 - Radial Chart in Action

--- WhiteLagoon.Web/wwwroot/js/Dashboard/getTotalBookingRadial.js
+++ WhiteLagoon.Web/wwwroot/js/Dashboard/getTotalBookingRadial.js
@@ -23,7 +23,68 @@ function loadTotalBookingRadialChart() {
 
             document.querySelector('#sectionBookingCount').append(sectionCurrentCount);
             document.querySelector('#sectionBookingCount').append('since last month');
+            loadRadialBarChart("totalBookingRadialChart", data);
             $('.chart-spinner').hide();
         }
     })
+}
+
+function loadRadialBarChart(id, data) {
+    var options = {
+        chart: {
+            height: 280,
+            type: "radialBar",
+        },
+
+        series: [67],
+        colors: ["#20E647"],
+        plotOptions: {
+            radialBar: {
+                hollow: {
+                    margin: 0,
+                    size: "70%",
+                    background: "#293450"
+                },
+                track: {
+                    dropShadow: {
+                        enabled: true,
+                        top: 2,
+                        left: 0,
+                        blur: 4,
+                        opacity: 0.15
+                    }
+                },
+                dataLabels: {
+                    name: {
+                        offsetY: -10,
+                        color: "#fff",
+                        fontSize: "13px"
+                    },
+                    value: {
+                        color: "#fff",
+                        fontSize: "30px",
+                        show: true
+                    }
+                }
+            }
+        },
+        fill: {
+            type: "gradient",
+            gradient: {
+                shade: "dark",
+                type: "vertical",
+                gradientToColors: ["#87D4F9"],
+                stops: [0, 100]
+            }
+        },
+        stroke: {
+            lineCap: "round"
+        },
+        labels: ["Progress"]
+    };
+
+    var chart = new ApexCharts(document.querySelector('#'+id), options);
+
+    chart.render();
+
 }
