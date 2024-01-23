9d845eda82b05241ba92c110fd980a51c2a7e328
roaksey <roaksey@gmail.com>
2024-01-23 13:03:39 +0545

    s16L204-207: PieChart with action implementation

--- WhiteLagoon.Web/Controllers/DashboardController.cs
+++ WhiteLagoon.Web/Controllers/DashboardController.cs
@@ -48,6 +48,22 @@ namespace WhiteLagoon.Web.Controllers
 
         }
 
+
+        public async Task<IActionResult> GetBookingPieChartData()
+        {
+            var totalBookings = _unitOfWork.Booking.GetAll(u => u.BookingDate >= DateTime.Now.AddDays(-30) && (u.Status != SD.StatusPending || u.Status != SD.StatusCancelled));
+            var customerWithOneBooking = totalBookings.GroupBy(b=>b.UserId).Where(c=>c.Count() == 1).Select(x=>x.Key).ToList();
+            int bookingsByNewCustomer = customerWithOneBooking.Count();
+            int bookingsByReturningCustomer = totalBookings.Count() - bookingsByNewCustomer;
+
+            PieChartVm pieChartVm = new()
+            {
+                Labels = new string[] { "New Customer Bookings", "Returning Customer Bookings" },
+                Series = new decimal[] { bookingsByNewCustomer, bookingsByReturningCustomer }
+            };
+            return Json(pieChartVm);
+        } 
+
         private static RadialBarChartVM GetRadialChartDataModel(int totalCount,double currentMonthCount,double prevMonthCount)
         {
             RadialBarChartVM radialBarChartVM = new();
